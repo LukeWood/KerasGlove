1c21239b292e46a315697cae2f89fd053ec28f49
Luke Wood <lswood@smu.edu>
2017-05-11 13:14:02 -0700

    ready to package

--- README.md
+++
@@ -1,6 +0,0 @@
-# KerasGlove
-KerasGlove is an open source extension on keras that makes it incredibly easy to use pretrained glove embeddings.
-
-Training your own embedding is incredibly expensive.  To avoid having to do this, the common approach is to use a pre-trained embedding.
-
-This package downloads and caches embeddings with 50, 100, 200 and 300 weights.
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
   packages = ['kerasglove'], # this must be the same as the name above
   version = '0.1',
   description = 'Pre-trained GLOVE embeddings for keras',
-  author = 'Luke Wood',
+  author = 'LukeWood',
   author_email = 'lukewoodcs@gmail.com',
   url = 'https://github.com/LukeWoodSMU/KerasGlove', # use the URL to the github repo
   download_url = 'https://github.com/LukeWoodSMU/KerasGlove/archive/0.1.tar.gz', # I'll explain this in a second

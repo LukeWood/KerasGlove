283582d5edfb50892c1b207ef72af4fe8b3a6369
Luke Wood <lswood@smu.edu>
2017-05-11 13:05:56 -0700

    commitment issues

--- __init__.py
+++ __init__.py
@@ -15,7 +15,43 @@ else:
 if not path.isdir(appdata):
     os.mkdir(appdata)
 
-def load_embedding_matrix(fname,word_index,EMBED_SIZE):
+def download_file(url,destination):
+    import requests
+    local_filename = destination
+    # NOTE the stream=True parameter
+    r = requests.get(url, stream=True)
+
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+                #f.flush() commented by recommendation from J.F.Sebastian
+    return local_filename
+
+# Download embeddings if neccesary
+if not os.exists(path.join(appdata,".downloaded")):
+    print("Downloading pre-trained glove embeddings to %s" % (appdata))
+    url = "http://nlp.stanford.edu/data/glove.6B.zip"
+    download_file(url,path.join(appdata,"glove.6B.zip"))
+    print("Unzipping glove embeddings.")
+    import zipfile
+    zip_ref = zipfile.ZipFile(path.join(appdata,"glove.6B.zip"))
+    zip_ref.extractall(appdata)
+    zip_ref.close()
+    print("Unzipping Complete: ",os.listdir(appdata),"were downloaded")
+    open(path.join(appdata,".downloaded"),"a").close()
+
+
+def GloveEmbedding(size,word_index,input_length,**kwargs):
+    if not size in [50,100,200,300]:
+        message = "Invalid Value %d passed as \"weights\" parameter.\n\tValid Values are: [50,100,200,300]"%num_weights
+        raise ValueError(message)
+
+    EMBED_SIZE = int(size)
+
+    fname = "glove.6B.%dd.txt"%size
+    fname = os.path.join(appdata,fname)
+
     embeddings_index = {}
     for line in open(fname):
         values = line.split()
@@ -29,19 +65,9 @@ def load_embedding_matrix(fname,word_index,EMBED_SIZE):
         if vec is not None:
             embedding_matrix[i] = vec
 
-def GloveEmbedding(size,word_index,input_length,**kwargs):
-    if not size in [50,100,200,300]:
-        message = "Invalid Value %d passed as \"weights\" parameter.\n\tValid Values are: [50,100,200,300]"%num_weights
-        raise ValueError(message)
-
-    EMBED_SIZE = int(size)
-
-    fname = "glove.6B.%dd.txt"%size
-    fname = os.path.join(appdata,fname)
-
     return Embedding(
         len(word_index)+1,
         EMBED_SIZE,
-        weights=[load_embedding_matrix(fname,word_index,EMBED_SIZE)],
+        weights=[embedding_matrix],
         input_length=input_length,
         trainable=False,**kwargs)
--- setup.py
+++ setup.py
@@ -1,45 +1,5 @@
 from distutils.core import setup
 
-import sys
-from os import path, environ
-import os
-
-#Get an appdata folder for our module.
-APPNAME = "KerasGlove"
-appdata = ""
-if sys.platform == 'win32':
-    appdata = path.join(environ['APPDATA'], APPNAME)
-else:
-    appdata = path.expanduser(path.join("~", "." + APPNAME))
-
-if not path.isdir(appdata):
-    os.mkdir(appdata)
-
-def download_file(url,destination):
-    import requests
-    local_filename = destination
-    # NOTE the stream=True parameter
-    r = requests.get(url, stream=True)
-
-    with open(local_filename, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=1024):
-            if chunk: # filter out keep-alive new chunks
-                f.write(chunk)
-                #f.flush() commented by recommendation from J.F.Sebastian
-    return local_filename
-
-print("Downloading pre-trained glove embeddings to %s" % (appdata))
-
-url = "http://nlp.stanford.edu/data/glove.6B.zip"
-download_file(url,path.join(appdata,"glove.6B.zip"))
-
-print("Unzipping glove embeddings.")
-import zipfile
-zip_ref = zipfile.ZipFile(path.join(appdata,"glove.6B.zip"))
-zip_ref.extractall(appdata)
-zip_ref.close()
-print("Unzipping Complete: ",os.listdir(appdata),"were downloaded")
-
 setup(
   name = 'kerasglove',
   packages = ['kerasglove'], # this must be the same as the name above
